15c956d5f75c411df6a0e834cb9eab752dfe2385
L0c_d0G <ruslanbadaev0@gmail.com>
2022-03-21 17:49:20 +0300

    Test

---
+++ main2.js
@@ -0,0 +1,17 @@
+function CBR_XML_Daily_Ru(rates) {
+    function trend(current, previous) {
+      if (current > previous) return ' ▲';
+      if (current < previous) return ' ▼';
+      return '';
+    }
+
+    let USDrat = rates.Valute.USD.Value.toFixed(2).replace('.', ',');
+    let US = document.getElementById('lastUSD');
+    US.innerHTML = US.innerHTML.replace('00,0000', USDrat);
+    // US.innerHTML += trend(rates.Valute.USD.Value, rates.Valute.USD.Previous);
+  
+    let EURrat = rates.Valute.EUR.Value.toFixed(2).replace('.', ',');
+    let EU = document.getElementById('lastEUR');
+    EU.innerHTML = EU.innerHTML.replace('00,0000', EURrat);
+    // EU.innerHTML += trend(rates.Valute.EUR.Value, rates.Valute.EUR.Previous);
+  }
